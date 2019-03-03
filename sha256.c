538187126f7281ba0f7b028126b733b87fe72a5a
kevgleeson78 <kevgleeson78@gmail.com>
2019-03-03 19:16:05 +0000

    video 2 finished!

--- sha256.c
+++ sha256.c
@@ -7,7 +7,7 @@
 //@Date 1/03/2019
 
 #include<stdio.h>
-#include<stdint>
+#include<stdint.h>
 
 void sha256();
 
@@ -79,50 +79,55 @@ void sha256(){
   };
   
   //Current Message Block array
-  uint32_t M[16];
+  uint32_t M[16] = {0, 0, 0, 0, 0, 0, 0 ,0};
   
   // Loop variable
-  int t;
-  // Wt = mt (Page 22)
-  for(t = 0; t < 16; t++){
+  int i, t;
+  //Loop throuh message blocks
+  for(i = 0; i < 1; i++){
+   // Wt = mt (Page 22)
+    for(t = 0; t < 16; t++){
     
-    W[t] = M[t];
+      W[t] = M[t];
     
-  }
+    }
 
 
-  // (page 22)
-  for(t = 16; t < 64; t++){
-    W[t] =  sig_1(W[t-2]) + W[t-7] + sig_0(W[t-15]) + W[t-16];
+    // (page 22)
+    for(t = 16; t < 64; t++){
+      W[t] =  sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
 
-    // Initialise a, b, c , d , e, f , h as per step 2, page 22.
-    a = H[0]; b = H[1]; c = H[2]; d = H[3];
-    e = H[4]; f = H[5]; g = H[6]; h= H[7];   
-  }
+      // Initialise a, b, c , d , e, f , h as per step 2, page 22.
+      a = H[0]; b = H[1]; c = H[2]; d = H[3];
+      e = H[4]; f = H[5]; g = H[6]; h= H[7];   
+    }
 
-  // Step 3
-  for( t = 0; t < 64; t++){
-    T1 = h + SIG1(e) + Ch(e,f,g) + K[t] + W[t];
-    T2 = SIG0(a) + Maj(a,b,c);
-    h = g;
-    g = f;
-    f = e;
-    e = d +T1;
-    d = c;
-    c = b;
-    b = a;
-    a = T1 + T2;        
-  }
+    // Step 3
+   for( t = 0; t < 64; t++){
+      T1 = h + SIG1(e) + Ch(e,f,g) + K[t] + W[t];
+      T2 = SIG0(a) + Maj(a,b,c);
+      h = g;
+      g = f;
+      f = e;
+      e = d +T1;
+      d = c;
+      c = b;
+      b = a;
+      a = T1 + T2;        
+    }
   
-  //Step 4
-  H[0] = a + H[0];
-  H[1] = b + H[1];
-  H[2] = c + H[2];
-  H[3] = d + H[3];
-  H[4] = e + H[4];
-  H[5] = f + H[5];
-  H[6] = g + H[5];
-  H[7] = h + H[7];
+    //Step 4
+    H[0] = a + H[0];
+    H[1] = b + H[1];
+    H[2] = c + H[2];
+    H[3] = d + H[3];
+    H[4] = e + H[4];
+    H[5] = f + H[5];
+    H[6] = g + H[6];
+    H[7] = h + H[7];
+  }
+
+  printf("%x %x %x %x %x %x %x %x\n", H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7], H[8]);
  }
 
 // See section 3.2 fro definitions.
@@ -146,7 +151,7 @@ uint32_t sig0(uint32_t x){
   // See Section 3.2 and 4.1.2 fro definitions
   // ROTR_n(x) = (x >> n) | (x << (32 -n))
   // SHR_n(x) = (x >> n)
-  reutrn (rotr(7, x) ^ rotr(18, x) ^ shr(3, x));
+  return (rotr(7, x) ^ rotr(18, x) ^ shr(3, x));
 
 }
 int32_t sig1(uint32_t x){
@@ -162,7 +167,7 @@ uint32_t SIG0(uint32_t x){
  return  (rotr(2, x) ^ rotr(13, x) ^ rotr(22, x));
 }
 uint32_t SIG1(uint32_t x){
-  return ((rotr(6, x) ^ rotr(11, x) ^ rotr(25, x));
+  return (rotr(6, x) ^ rotr(11, x) ^ rotr(25, x));
 }
 
 uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
